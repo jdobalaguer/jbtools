820fbfaf25a28e09750762ef4615d1036970d4f6
jan <jan@deepmind.com>
2014-03-25 14:11:49 +0000

    mturk_parseurl for both data and tmp

--- mturk_parse/mturk_parseurl.m
+++ mturk_parse/mturk_parseurl.m
@@ -1,9 +1,21 @@
 
-function mturk_parseurl(path,url)
-    %% get url
-    mturk_geturl(path,url);
+function mturk_parseurl(path_file,path_url)
+    if path_file(end)=='/', path_file(end) = []; end
+    if path_url(end)=='/',  path_url(end) = [];  end
     
+    %% set paths
+    url = {};
+    url{1} = [path_url,'/data'];
+    url{2} = [path_url,'/tmp'];
+    file = {};
+    file{1} = [path_file,'/data'];
+    file{2} = [path_file,'/tmp'];
+    
+    %% get url
+    mturk_geturl(file{1},url{1});
+    mturk_geturl(file{2},url{2});
+
     %% parse data
-    mturk_parseall(path);
+    mturk_parseall(file{1});
     
 end
