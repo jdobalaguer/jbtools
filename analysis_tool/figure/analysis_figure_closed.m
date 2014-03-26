97b7a90defe17f68148cf30b2e690bd09d0663c7
jan <jan@deepmind.com>
2014-03-26 09:47:07 +0000

    fix analysis_figure_closed

--- analysis_tool/figure/analysis_figure_closed.m
+++ analysis_tool/figure/analysis_figure_closed.m
@@ -4,16 +4,15 @@
 ii_fig = [];
 
 nb_fig = length(obj.fig);
-u_fig  = cell(1,nb_fig);
 for i_fig = 1:nb_fig
+    obj.fig
     if (obj.fig{i_fig}.handle == gcbf)
         ii_fig(end+1) = i_fig;
     end
 end
 
 %% remove figures
-u_fig(ii_fig) = [];
-obj.fig = u_fig;
+obj.fig(ii_fig) = [];
 
 %% refresh gui
 obj.obj.figure.refresh();
