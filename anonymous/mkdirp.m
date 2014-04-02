18ed4157d3aa200a8e3810a2e4adc8baee375ebb
jan <jan@deepmind.com>
2014-04-02 14:23:26 +0100

    mkdirp working under windows

--- anonymous/mkdirp.m
+++ anonymous/mkdirp.m
@@ -1,9 +1,8 @@
 
 function mkdirp(path)
-    assert(~ispc(),'mkdirp: doesnt work under win');
-    if path(1  )~='/'; path = [pwd(),filesep(),path]; end
-    if path(end)=='/'; path(end)=[];                  end
-    i_filesep = find(path=='/',1,'last');
+    if path(1  )~=filesep(); path = [pwd(),filesep(),path]; end
+    if path(end)==filesep(); path(end)=[];                  end
+    i_filesep = find(path==filesep(),1,'last');
     rootpath = path;
     rootpath(i_filesep:end) = [];
     if ~exist(rootpath,'dir'); mkdirp(rootpath); end
