c9c4f84b794636129becb7c14242482efb1363b3
jan <jdobalaguer@gmail.com>
2015-02-05 00:13:46 +0000

    fix model_cost_binary

--- model/model_cost_binary.m
+++ model/model_cost_binary.m
@@ -7,7 +7,8 @@ function cost = model_cost_binary(data,simu,pars)
     %% assert
     
     %% function
-    v_data = mat2vec(data.(pars.data));
+    v_data = mat2vec(logical(data.(pars.data)));
     v_simu = mat2vec(simu.(pars.simu));
-    cost = geomean([1 - v_simu(v_data);v_simu(~v_data)]);
+    prob   = [1 - v_simu(v_data);v_simu(~v_data)];
+    cost = geomean(0.01 + 0.99*prob);
 end
