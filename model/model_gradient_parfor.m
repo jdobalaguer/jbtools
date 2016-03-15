b1049907e75f5af0d6d03125e84890685869658d
jan <jdobalaguer@gmail.com>
2016-03-15 07:33:25 +0000

    automatic upload

--- model/model_gradient_parfor.m
+++ model/model_gradient_parfor.m
@@ -8,9 +8,12 @@ function result = model_gradient_parfor(problem,parfor_x0,parfor_simu_pars,parfo
     problem.objective = @cost_function;
 
     % fminsearch
-    result = struct();
-    [result.u_min,result.v_min,exitflag] = fminsearch(problem);
-    assert(exitflag > 0,'model_gradient: error. limit iteration reached \n');
+    result   = struct('u_min',{nan(size(parfor_x0))},'v_min',{inf});
+    exitflag = true;
+    try     [result.u_min,result.v_min,exitflag] = fminsearch(problem);
+    catch,  exitflag = -1;
+    end
+    assertWarning(exitflag > 0,'model_gradient_parfor: error. fminsearch \n');
 
     %% nested
     function cost = cost_function(x)
