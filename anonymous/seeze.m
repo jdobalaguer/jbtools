fb3475355e7f7fb741e0cfe21e27f1c558fbc938
jan <jdobalaguer@gmail.com>
2017-07-14 11:17:54 +0100

    automatic upload

--- anonymous/seeze.m
+++ anonymous/seeze.m
@@ -1,16 +1,16 @@
 
-function y = seeze(x,dim)
-    %% y = seeze(x,dim)
+function y = seeze(x,d)
+    %% y = seeze(x[,d])
+    % get the sum, and squeeze through those dimensions
+    % x : matrix. input values
+    % d : vector. dimensions to remove
     
     %% function
-    if nargin==1
-        y = squeeze(nansum(x));
-    else
-        y = x;
-        n = length(dim);
-        for i=1:n,
-            y = nansum(y,dim(i));
-        end
-        y = squeeze(y);
+    func_default('d',1);
+    y = x;
+    n = length(d);
+    for i=1:n
+        y = nansum(y,d(i));
     end
+    y = mat_squeeze(y,d);
 end
